9b104c612a88e3dbb4b16c88c2ff9524efb55859
Kihyeon <abata1st@gmail.com>
2019-10-17 08:48:11 +0900

    Update vim_setup.sh

--- vim_setup.sh
+++ vim_setup.sh
@@ -7,28 +7,28 @@ sudo apt upgrade -y
 sudo apt remove vim vim-runtime gvim -y
 
 sudo apt install -y libncurses5-dev \
-				    libncursesw5-dev \
-				    libgnome2-dev \
-				    libgnomeui-dev \
-				    libgtk2.0-dev \
-				    libatk1.0-dev \
-				    libbonoboui2-dev \
-				    libcairo2-dev \
-				    libx11-dev \
-				    libxpm-dev \
-				    libxt-dev \
-				    build-essential \
-				    python-dev\
-				    python3-dev \
-				    ruby-dev \
-				    lua5.1 \
-				    liblua5.1-dev \
-				    libperl-dev \
-				    git \
-				    wget \
-					ctags \
-					cscope \
-					npm
+                    libncursesw5-dev \
+		    libgnome2-dev \ 
+		    libgnomeui-dev \
+		    libgtk2.0-dev \
+		    libatk1.0-dev \
+		    libbonoboui2-dev \
+		    libcairo2-dev \
+		    libx11-dev \
+		    libxpm-dev \
+		    libxt-dev \
+		    build-essential \
+		    python-dev\
+		    python3-dev \
+		    ruby-dev \
+		    lua5.1 \
+		    liblua5.1-dev \
+		    libperl-dev \
+		    git \
+		    wget \
+		    ctags \
+		    cscope \
+		    npm
 
 git clone https://github.com/vim/vim.git
 
@@ -37,7 +37,17 @@ cd ./vim
 PYTHON2VER=$(python2 --version | awk '{print $2}' | awk -F. '{print $1"."$2}')
 PYTHON3VER=$(python3 --version | awk '{print $2}' | awk -F. '{print $1"."$2}')
 
-./configure --with-features=huge --enable-multibyte --enable-pythoninterp=yes --with-python-config-dir=/usr/lib/python${PYTHON2VER}/config-x86_64-linux-gnu --enable-python3interp=yes --with-python3-config-dir=/usr/lib/python${PYTHON3VER}/config-${PYTHON3VER}m-x86_64-linux-gnu --enable-perlinterp=yes --enable-gui=gtk2 --enable-cscope --prefix=/usr/local --with-tlib=ncurses
+./configure --with-features=huge \
+            --enable-multibyte \
+	    --enable-pythoninterp=yes \
+	    --with-python-config-dir=/usr/lib/python${PYTHON2VER}/config-x86_64-linux-gnu \
+	    --enable-python3interp=yes \
+	    --with-python3-config-dir=/usr/lib/python${PYTHON3VER}/config-${PYTHON3VER}m-x86_64-linux-gnu \
+	    --enable-perlinterp=yes \
+	    --enable-gui=gtk2 \
+	    --enable-cscope \
+	    --prefix=/usr/local \
+	    --with-tlib=ncurses
 
 make VIMRUNTIMEDIR=/usr/local/share/vim/vim81
 sudo apt install checkinstall
@@ -76,7 +86,6 @@ echo '---------------------------- clang installation'
 UBUNTU_VERSION=$(lsb_release -a 2>/dev/null | grep Release | awk '{print $2}')
 CLANG_BINARY="clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-${UBUNTU_VERSION}"
 wget http://releases.llvm.org/9.0.0/$CLANG_BINARY.tar.xz
-CLANG_BINARY="clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-${UBUNTU_VERSION}"
 xz -d $CLANG_BINARY.tar.xz
 tar xf $CLANG_BINARY.tar
 
